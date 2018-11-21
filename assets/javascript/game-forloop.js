2b02a869819ac14122e22df1b5a8981096031350
Richard Fitkin <rfitkin90@gmail.com>
2018-11-21 15:27:23 -0800

    fixed kefka css

--- assets/javascript/game-forloop.js
+++ assets/javascript/game-forloop.js
@@ -48,39 +48,75 @@ $(document).ready(function () {
     var enemyCharacterSelectPhase = false;
     var battlePhase = false;
 
-    // add onclick event to portraits to select player and computer characters
-    // and create battle-sprite images and attack button
 
     $(".portrait").hover(function () {
         $(this).css({ 'cursor': 'pointer' });
     });
 
+    // add onclick event to portraits to select player and computer characters
+    // and create battle-sprite images and attack button
     for (var i = 0; i < characterArray.length; i++) {
+
+        // directly links the spriteID/Src of each object to its sibling portraitId
         $(characterArray[i].portraitID).attr("data-sprite-id", characterArray[i].spriteID);
         $(characterArray[i].portraitID).attr("data-sprite-src", characterArray[i].spriteSrc);
+
+        // when clicking the portrait
         $(characterArray[i].portraitID).click(function () {
+
+            //checks if the player character select phase is active
             if (playerCharacterSelectPhase === true) {
 
+                // fades out the portrait of the character picked
                 $(this).css("opacity", "0.5");
 
-                var sprite = $(`<img id = "${$(this).attr("data-sprite-id")}" src = "${$(this).attr("data-sprite-src")}">`);
+                // creates the sprite w/ the ID and Src belonging to the same object as the clicked portrait
+                var playerSprite = $(`<img id = "${$(this).attr("data-sprite-id")}" src = "${$(this).attr("data-sprite-src")}">`);
 
-                $("#player-position").append(sprite);
-                sprite.css({
+                // appends the sprite to player position(on the left)
+                $("#player-position").append(playerSprite);
+
+                // gives some css to playerSprite
+                playerSprite.css({
+                    // flips image horizontally
                     "transform": "scaleX(-1)",
+                    // positions sprite relative to parent div
                     "position": "absolute",
+                    // puts sprite at bottom of div so sprites of different height are standing in the same spot
                     "bottom": 0,
                 });
+                // kefka requires unique css since his sprite is larger and facing the other direction compared to the 3 other sprites
                 $("#kefka-sprite").css({
                     "transform": "scaleX(1)",
                     "bottom": -20,
                     "left": -30,
                 });
+
                 $("#left-ui-text").text("Choose opponent");
+
+                // ends player select phase and begins enemy select phase
                 playerCharacterSelectPhase = false;
                 enemyCharacterSelectPhase = true;
+
+                // if player has already selected a character
             } else if (enemyCharacterSelectPhase === true) {
 
+                var enemySprite = $(`<img id = "${$(this).attr("data-sprite-id")}" src = "${$(this).attr("data-sprite-src")}">`);
+
+                $("#enemy-position").append(enemySprite);
+
+                enemySprite.css({
+                    "position": "absolute",
+                    "bottom": 0,
+                });
+                
+                $("#left-ui-text").hover(function () {
+                    $(this).css({ 'cursor': 'pointer' });
+                });
+                $("#left-ui-text").text("Click Here to Attack");
+
+                enemyCharacterSelectPhase = false;
+                battlePhase = true;
             }
         });
     }
